d9bae0e5152a8e1d44cffe0265d8a04bf66b217a
rohansalakhanar <slakhanar@gmail.com>
2024-02-06 14:27:21 +0530

    scaffold and container

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,7 +11,49 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text("+"),
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 200,
+            width: 200,
+            decoration: BoxDecoration(
+              color: Colors.yellow,
+              border: Border.all(),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.green,
+                  offset: Offset(0.0, 10.0),
+                  blurRadius: 10.0,
+                  spreadRadius: 2.0,
+                ),
+              ],
+              shape: BoxShape.circle,
+              gradient: LinearGradient(
+                begin: Alignment.bottomLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Colors.red,
+                  Colors.yellow,
+                ],
+              ),
+              // borderRadius: BorderRadius.only(),
+            ),
+            child: Center(child: Text("Skill")),
+          ),
+          Container(
+            height: 40,
+            width: 50,
+            color: Colors.black,
+          )
+        ],
+      ),
     );
   }
 }
