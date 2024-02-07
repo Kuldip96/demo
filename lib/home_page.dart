2796b01c3515ac9e7e8288176e52fe707362f7b3
rohansalakhanar <slakhanar@gmail.com>
2024-02-07 14:27:21 +0530

    text

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,49 +10,64 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text("+"),
-      ),
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-      ),
-      body: Column(
-        children: [
-          Container(
-            height: 200,
-            width: 200,
-            decoration: BoxDecoration(
-              color: Colors.yellow,
-              border: Border.all(),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.green,
-                  offset: Offset(0.0, 10.0),
-                  blurRadius: 10.0,
-                  spreadRadius: 2.0,
-                ),
-              ],
-              shape: BoxShape.circle,
-              gradient: LinearGradient(
-                begin: Alignment.bottomLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Colors.red,
-                  Colors.yellow,
+    return SafeArea(
+      bottom: false,
+      left: false,
+      child: Scaffold(
+        backgroundColor: Colors.blue,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text("+"),
+        ),
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+        ),
+        body: Column(
+          children: [
+            Container(
+              height: 200,
+              width: 200,
+              decoration: BoxDecoration(
+                color: Colors.yellow,
+                border: Border.all(),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.green,
+                    offset: Offset(0.0, 10.0),
+                    blurRadius: 10.0,
+                    spreadRadius: 2.0,
+                  ),
                 ],
+                shape: BoxShape.circle,
+                gradient: LinearGradient(
+                  begin: Alignment.bottomLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Colors.red,
+                    Colors.yellow,
+                  ],
+                ),
+                // borderRadius: BorderRadius.only(),
               ),
-              // borderRadius: BorderRadius.only(),
+              child: Center(
+                child: Text(
+                  "Skill c",
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 30,
+                      letterSpacing: 10,
+                      wordSpacing: 10,
+                      fontWeight: FontWeight.w800),
+                ),
+              ),
             ),
-            child: Center(child: Text("Skill")),
-          ),
-          Container(
-            height: 40,
-            width: 50,
-            color: Colors.black,
-          )
-        ],
+            Container(
+              height: 40,
+              width: 50,
+              color: Colors.black,
+            ),
+          ],
+        ),
       ),
     );
   }
